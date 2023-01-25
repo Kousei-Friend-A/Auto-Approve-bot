bfcc6de67f224aa65004f9b5aa042f4798725d31
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2023-01-25 20:12:23 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
+    API_ID = 17945796
+    API_HASH = "4a05481a5da2d66f801acffc4ca5ee4b"
+    BOT_TOKEN = "5700920410:AAEk8j5NZb6wiIVuu3-NS6AL243bl57EpvU"
     FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
-    
+    CHID = -1001874063925
+    SUDO = 5152809878
+    MONGO_URI = "mongodb+srv://manu911:manu911@cluster0.frtlbqt.mongodb.net/?retryWrites=true&w=majority"
+        
 cfg = Config()
